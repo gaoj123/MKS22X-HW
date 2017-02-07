8817067d74f966aa0a1dc6245b5a2c1c2f85f2f6
Jenny Gao <jgao658@gmail.com>
2017-02-06 20:46:55 -0500

    created a file with square root function

---
+++ 01Recursion/Recursion.java
@@ -0,0 +1,19 @@
+public class Recursion{
+    public static String name(){
+	return "Gao,Jenny";
+    }
+    public static double sqrt(double n){
+	return helper(n,1);
+    }
+    public static double helper(double n, double guess){
+	if(Math.abs((Math.pow(guess,2)-n)/n)<=0.00000000001){
+	    return guess;
+	}
+	else{
+	    return helper(n,(n/guess+guess)/2);
+	}
+    }
+    public static void main(String[] args){
+	System.out.println(sqrt(57));
+    }
+}
