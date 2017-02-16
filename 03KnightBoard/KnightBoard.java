6e6b39963902d3dcdd45e698ea67d12d40eb279a
Jenny Gao <jgao658@gmail.com>
2017-02-16 12:31:08 -0500

    created file

---
+++ 03KnightBoard/KnightBoard.java
@@ -0,0 +1,13 @@
+public class KnightBoard(){
+    public boolean solveH(int row, int col, int level){
+	if(level>row*col){
+	    return true;
+	}
+	else{
+
+	    if(isOnGoodSpot(row,col){
+		    board[row][col]=level;
+		}
+	}
+    }
+}
