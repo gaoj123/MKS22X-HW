fc8ae82f8b67d237ebe01f59fb96ebb5e0cd2e1b
Jenny Gao <jgao658@gmail.com>
2017-03-02 12:29:33 -0500

    commented some lines

--- 04Maze/Driver.java
+++ 04Maze/Driver.java
@@ -1,12 +1,11 @@
 public class Driver{
 
     public static void main(String[]args){
-        Maze f;
-        f = new Maze("data1.dat");//true animates the maze.
+        Maze f = new Maze("data1.txt");//true animates the maze.
         
-        f.setAnimate(true);
-        f.solve();
+        //f.setAnimate(true);
+        //f.solve();
 
-        System.out.println(f);
+        //System.out.println(f);
     }
 }
