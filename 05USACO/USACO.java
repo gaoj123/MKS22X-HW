2a6076954481ba4e2fae70b6b6bbb592cc72e0df
Jenny Gao <jgao658@gmail.com>
2017-03-12 22:56:15 -0400

    removed comments

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -98,46 +98,10 @@ public class USACO{
 	System.out.println(rows);
 	System.out.println(cols);
 	System.out.println(time);
-	// while(lineNum.hasNextLine()){
-	// 	String line=lineNum.nextLine();
-	// 	if(line.length()==5){
-	// 	    int rows=Integer.parseInt(line.substring(0,1));
-	// 	    int cols=Integer.parseInt(line.substring(2,3));
-	// 	    int time=Integer.parseInt(line.substring(4,5));
-	// 	}
-	// }
-	//	char[][] maze =new char[rows][cols];
 	int[][][] board=new int[rows][cols][2];
-	//int onLineNumber=0;
-	//Scanner inf=new Scanner(infile);
-	// while(inf.hasNextLine()){
-	// 	String line = inf.nextLine();
-	// 	if(line.length()==7){
-	// 	    int startR=Integer.parseInt(line.substring(0,1));
-	// 	    int startC=Integer.parseInt(line.substring(2,3));
-	// 	    int endR=Integer.parseInt(line.substring(4,5));
-	// 	    int endC=Integer.parseInt(line.substring(6,7));
-	// 	}
-	// 	else{
-	// 	    for(int i=0;i<line.length();i++){
-	// 		maze[onLineNumber][i]=line.charAt(i);
-	// 	    }
-	// 	    onLineNumber+=1;
-	// 	}
-	// }
-	// System.out.println(rows);
-	// System.out.println(cols);
 	for(int i=0;i<rows;i++){
 	    String lineOn=lineNum.next();
 	    for(int j=0;j<cols;j++){
-		//Scanner onNow=new Scanner(lineNum.nextLine());
-		// System.out.println(onNow);
-		//String now=onNow.next();
-		//System.out.println(now);
-		//maze[i][j]=now.charAt(0);
-		//System.out.println(lineNum.next());
-		//String lineOn=lineNum.next();
-		//System.out.println(lineOn.substring(j,j+1));
 		if(lineOn.substring(j,j+1).equals("*")){
 		    board[i][j][1]=-1;
 		}
@@ -146,53 +110,15 @@ public class USACO{
 		}
 	    }
 	}
-	//Scanner last=new Scanner(lineNum.nextLine());
 	int startR=lineNum.nextInt();
 	int startC=lineNum.nextInt();
 	int endR=lineNum.nextInt();
 	int endC=lineNum.nextInt();
 	return solveH(board,startR,startC,endR,endC,time,rows,cols);
-	    //solve();
-	    //count=countSols();
-	    //return count;
-	// catch (FileNotFoundException e) {
-	//     System.out.println("File not found!");
-	//     System.exit(0);
-	// }
     }
     public static int solveH(int[][][] pasture, int rStart, int cStart, int rEnd, int cEnd, int timee, int rowsS,int colsS){
-	//	int[][] pastureCurrent=new int[pasture.length][pasture[0].length];
-	// for(int i=0;i<pasture.length;i++){
-	//     for(int j=0;j<pasture[0].length;j++){
-	// 	pastureCurrent[i][j]=pasture[i][j];
-	// 	System.out.print(pasture[i][j]);
-	//     }
-	//     System.out.println("\n");
-	// }
-	// pasture[rStart-1][cStart-1]=1;
-	// pastureCurrent[rStart-1][cStart-1]=1;
 	pasture[rStart-1][cStart-1][1]=1;
 	for(int stepNow=0;stepNow<timee;stepNow++){
-	    // for(int i=0;i<pasture.length;i++){
-	    // 	for(int j=0;j<pasture[0].length;j++){
-	    // 	    pastureCurrent[i][j]=pasture[i][j];
-	    // 	    //System.out.print(pasture[i][j]);
-	    // 	}
-	    // 	//System.out.println("\n");
-	    // }
-	    // // int[][] pastureCurrent=new int[pasture.length][pasture[0].length];
-	    // // for(int i=0;i<pasture.length;i++){
-	    // // 	for(int j=0;j<pasture[0].length;j++){
-	    // // 	    pastureCurrent[i][j]=pasture[i][j];
-	    // // 	}
-	    // // }
-	     // if(stepNow==0){
-	     // 	 pasture[rStart-1][cStart-1][0]=1;
-	     // 	 pasture[rStart-1][cStart-1][1]=1;
-	     // }
-	    // // 	return pasture[rEnd-1][cEnd-1];
-	    // // }
-	    //else{
 		for(int r=0;r<pasture.length;r++){
 		    for(int c=0;c<pasture[0].length;c++){
 			if(pasture[r][c][1]!=-1){
@@ -230,63 +156,15 @@ public class USACO{
 
 		}
 	}
-	//}
 	return pasture[rEnd-1][cEnd-1][1];
     }
-    // public boolean solve(){
-    // 	int startr=-1,startc=-1;
-    // 	startr=startR;
-    // 	startc=startC;
-    // 	//Initialize starting row and startint col with the location of the S. 
-
-    // 	maze[startr][startc] = ' ';//erase the S, and start solving!
-    // 	if(!solve(startr,startc,0)){
-    // 	    System.out.println("No Solution\n");
-    // 	    return false;
-    // 	}
-    // 	else{
-    // 	    return true;
-    // 	}
-    // }
     public static boolean isOnBoard(int rowss, int cowss, int r, int c){
     	return ((r>=0&&r<rowss)&&(c>=0&&c<cowss));
     }
-    // private boolean solve(int row, int col, int moves){
-    // 	if(isOnBoard(row,col)){
-    // 	    if(row==endR&&col==endC&&moves==time){
-    // 		return true;
-    // 	    }
-    // 	    if(maze[row][col]=='*'||maze[row][col]=='!'){
-    // 		return false;
-    // 	    }
-    // 	    else if(maze[row][col]=='.'){
-    // 		maze[row][col]='@';
-    // 		if(solve(row,col+1,moves+1)){
-    // 		    return true;
-    // 		}
-    // 		if(solve(row+1,col,moves+1)){
-    // 		    return true;
-    // 		}
-    // 		if(solve(row,col-1,moves+1)){
-    // 		    return true;
-    // 		}
-    // 		if(solve(row-1,col,moves+1)){
-    // 		    return true;
-    // 		}
-    // 		maze[row][col]='!';
-    // 		return false;
-    // 	    }
-    // 	    return false;
-    // 	}
-    // 	return false;
-	    
-    // }
-    // public int countSols(){
-    // }
     //testing
     public static void main(String[]args){
-	USACO x = new USACO();
-	System.out.println(x.silver("file4.txt"));
+	//USACO x = new USACO();
+	//System.out.println(x.silver("file4.txt"));
 	// System.out.println(x.bronze("infile2.txt"));
 	// System.out.println(x.bronze("infile3.txt"));
 	// System.out.println(x.bronze("infile4.txt"));
