8a80ff2d6ca987c0d6853ffd89ceddd634751ee1
Jenny Gao <jgao658@gmail.com>
2017-03-24 10:35:07 -0400

    new file

---
+++ 08Merge/Merge.java
@@ -0,0 +1,9 @@
+public class Merge{
+    public static String name(){
+	return "Gao,Jenny";
+    }
+    public static void mergesort(int[] ary){
+    }
+    public void merge(int[] a, int[] b, int[] destination){
+    }
+}
