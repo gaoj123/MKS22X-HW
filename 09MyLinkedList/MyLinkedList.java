ec767cd32d7a4ca3f2e041d04e9ac7d6bc451fba
Jenny Gao <jgao658@gmail.com>
2017-03-28 12:28:58 -0400

    inner class LNode

--- 09MyLinkedList/MyLinkedList.java
+++ 09MyLinkedList/MyLinkedList.java
@@ -1,2 +1,30 @@
 public class MyLinkedList{
+    LNode start;
+    int size;
+    class LNode{
+	int val;
+	LNode next;
+	public LNode(int value){
+	    val=value;
+	}
+	public LNode(int value, LNode nex){
+	    val=value;
+	    next=nex;
+	}
+    }
+    public String toString(){
+	String toRet="[";
+	LNode current=start;
+	while(current.next!=null){
+	    toRet+=current.val;
+	    toRet+=",";
+	    current=current.next;
+	}
+	toRet+="]";
+	return toRet;
+    }
+    public static void main(String[] args){
+	LNode a=new LNode(5);
+	LNode b=new LNode(3,a);
+    }
 }
