ab29f0a3b375ac1cc4b3f2ec100fd9c07f3034f3
Jenny Gao <jgao658@gmail.com>
2017-05-03 12:31:42 -0400

    not working, just method headers

---
+++ 13Heap/MyHeap.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class MyHeap{
+    private boolean minMax;
+    public MyHeap(){
+	minMax=true;
+    }
+    public MyHeap(boolean maxMin){
+	minMax=maxMin;
+    }
+    public void add(String s){
+    }
+    public String remove(){
+    }
+    public String peek(){
+    }
+    public void pushForth(){
+    }
+    public void pushBack(){
+    }
+}
