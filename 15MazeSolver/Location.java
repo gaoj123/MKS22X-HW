44e2037b867467cb64fe6bf32a9881a2d0ae36b7
Jenny Gao <jgao658@gmail.com>
2017-05-16 11:59:54 -0400

    new file

---
+++ 15MazeSolver/Location.java
@@ -0,0 +1,11 @@
+public class Location implements Comparable<Location>{
+    private int row,col;
+    private int distToGoal;
+    private int distToStart;
+    private Location previous;
+    private boolean aStar;
+    public Location(int r,int c,Location previous,int distToStart, int distToGoal,boolean aStart){
+    }
+    public CompareTo(Location other){
+    }
+}
