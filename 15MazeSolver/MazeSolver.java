3d14f674a254649a2df9fac13d780a22c85d046b
Jenny Gao <jgao658@gmail.com>
2017-05-16 12:03:49 -0400

    new file

---
+++ 15MazeSolver/MazeSolver.java
@@ -0,0 +1,15 @@
+public class MazeSolver{
+    public MazeSolver(String filename){
+	this(filename,false);
+	//done
+    }
+    public MazeSolver(String filename, boolean animate){
+    }
+    public void solve(){
+	solve(1);
+    }
+    public void solve(int style){
+    }
+    public String toString(){
+    }
+}
