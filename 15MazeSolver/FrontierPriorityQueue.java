798ec92f160e84dcd71e9e01149c222d92af2aff
Jenny Gao <jgao658@gmail.com>
2017-05-18 12:07:36 -0400

    added instance var and method signatures

--- 15MazeSolver/FrontierPriorityQueue.java
+++ 15MazeSolver/FrontierPriorityQueue.java
@@ -1,2 +1,7 @@
 public class FrontierPriorityQueue implements Frontier{
+    PriorityQueue<Location> locations=PriorityQueue<Location>;
+    public void add(Location a){
+    }
+    public Location next(){
+    }
 }
