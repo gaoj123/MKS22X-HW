f433021b804f6c45571ebe95e255c1bb7c9447b3
Jenny Gao <jgao658@gmail.com>
2017-05-18 12:19:21 -0400

    linked list and method signatures

--- 15MazeSolver/FrontierQueue.java
+++ 15MazeSolver/FrontierQueue.java
@@ -1,2 +1,8 @@
 public class FrontierQueue implements Frontier{
+    LinkedList<Location> locations=new LinkedList<Location>();
+    public void add(Location x){
+	locations.add(x);
+    }
+    public Location next(){
+    }
 }
