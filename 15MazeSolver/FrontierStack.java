4e2503e1f27ddbc959cd7c9e2272876fde4fa9a5
Jenny Gao <jgao658@gmail.com>
2017-05-18 12:10:37 -0400

    added instance var and method signatures

--- 15MazeSolver/FrontierStack.java
+++ 15MazeSolver/FrontierStack.java
@@ -1,2 +1,8 @@
+import java.util.*;
 public class FrontierStack implements Frontier{
+    Stack<Location> locations=new Stack<Location>();
+    public void add(Location x){
+    }
+    public Location next(){
+    }
 }
