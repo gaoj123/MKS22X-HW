3d9be97cfca9e1ed256dc5c3f4d8bd3e79b62abd
Jenny Gao <jgao658@gmail.com>
2017-05-21 15:20:38 -0400

    added methods

--- 15MazeSolver/FrontierQueue.java
+++ 15MazeSolver/FrontierQueue.java
@@ -1,8 +1,20 @@
+import java.util.*;
 public class FrontierQueue implements Frontier{
     LinkedList<Location> locations=new LinkedList<Location>();
-    public void add(Location x){
-	locations.add(x);
+    public FrontierQueue(){
+    }
+    public Location peek(){
+	return locations.peek();
+    }
+    public void add(Location loc){
+	locations.add(loc);
     }
     public Location next(){
+	Location toRet=locations.remove();
+	return toRet;	
+    }
+    public boolean hasNext(){
+	boolean x=locations.peek()==null;
+	return !x;
     }
 }
