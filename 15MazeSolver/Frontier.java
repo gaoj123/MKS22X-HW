37e758dbac9f9dedc60b705c05f7950fa9f4f2f6
Jenny Gao <jgao658@gmail.com>
2017-05-22 22:43:34 -0400

    added size

--- 15MazeSolver/Frontier.java
+++ 15MazeSolver/Frontier.java
@@ -2,6 +2,6 @@ public interface Frontier{
     public boolean hasNext();
     public Location next();
     public void add(Location loc);
-    public boolean hasNext();
     public Location peek();
+    public int size();
 }
